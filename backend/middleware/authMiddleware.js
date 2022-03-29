370fb33ed5c775311ad5e3f79eb8989c88e8e623
wasifraza909 <wasifraza909@gmail.com>
2022-03-29 15:02:55 +0500

    Backend completed

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -19,8 +19,6 @@ const protect = asyncHandler(async (req, res, next) => {
       // Get user from token
       req.user = await User.findById(decoded.id).select('-password');
 
-      console.log(req.user);
-
       next();
     } catch (error) {
       console.log(error);
